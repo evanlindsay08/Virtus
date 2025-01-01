0331052b53129776947eeeaa4d126c063a33ad48
evanlindsay08 <evanlindsay06@gmail.com>
2025-01-01 22:28:45 +0000

    Restored layout and styling configurations

--- next.config.js
+++ next.config.js
@@ -25,7 +25,15 @@ const nextConfig = {
     forceSwcTransforms: true
   },
   poweredByHeader: false,
-  generateEtags: false
+  generateEtags: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      type: 'asset/resource',
+    });
+    return config;
+  },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,25 @@
 import './globals.css'
+import { Inter } from 'next/font/google'
+import NavBar from '../components/NavBar'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Virtus',
+  description: 'Redefining The Deployment Of AI Agents',
+  icons: {
+    icon: '/Favicon.png',
+  },
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className={inter.className}>
+        <NavBar />
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   )
